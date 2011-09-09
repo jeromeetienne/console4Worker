42bf468f756890197e27bd005389994bfc00ef1f
Jerome Etienne <jerome.etienne@gmail.com>
2011-09-09 16:58:27 +0200

    major refactoring. less naive implementation. console.dir better. no trace yet

--- examples/worker.js
+++ examples/worker.js
@@ -2,5 +2,9 @@ importScripts('../console4Worker-worker.js');
 
 console.log("Server says hi")
 
+console.assert('something true', "wow");
+
+console.assert('', "wow");
+
 self.postMessage("foobar not from console.*");
 
